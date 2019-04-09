a1ddcce964ed9bd923882847c4c1491358bf61c2
Guilherme Cardoso <gchicoper@gmail.com>
2019-04-08 23:49:45 -0300

    Started adding some BIG BOYOS

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 CC = gcc
-CFLAGS = -v -std=c11 -Wall -Wextra -pedantic -Wmissing-prototypes -Wstrict-prototypes \
+CFLAGS = -std=c11 -Wall -Wextra -pedantic -Wmissing-prototypes -Wstrict-prototypes \
     -Wold-style-definition -O3
 
 CPPC = g++
 DEBUG_FLAGS = -g -D _DEBUG
-CPPFLAGS = -v -std=c++17 -Wall -Wextra  -Wstrict-aliasing -pedantic -fmax-errors=5 -Werror -Wunreachable-code -Wcast-align -Wcast-qual -Wctor-dtor-privacy\
+CPPFLAGS = -std=c++17 -Wall -Wextra  -Wstrict-aliasing -pedantic -fmax-errors=5 -Werror -Wunreachable-code -Wcast-align -Wcast-qual -Wctor-dtor-privacy\
  -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wmissing-include-dirs -Wnoexcept -Wold-style-cast -Woverloaded-virtual -Wredundant-decls -Wshadow \
  -Wsign-promo -Wstrict-null-sentinel -Wstrict-overflow=5 -Wswitch-default -Wundef -Wno-unused -Wno-variadic-macros -Wno-parentheses -fdiagnostics-show-option\
  -O3
@@ -23,11 +23,12 @@ otp:
 
 xorshift_debug:
 	mkdir -p bin
-	${CPPC} xorshift/xorshift.cpp -o bin/xorshift ${DEBUG_FLAGS} ${CPPFLAGS}
+	${CPPC} xorshift/xorshift.cpp -o bin/xorshift ${DEBUG_FLAGS}\
+	 ${CPPFLAGS} 
 
 xorshift_release:
 	mkdir -p bin
-	${CPPC} xorshift/xorshift.cpp -o bin/xorshift ${CPPFLAGS}
+	${CPPC} xorshift/xorshift.cpp -o bin/xorshift ${CPPFLAGS} 
 
 multiply_carry_debug:
 	mkdir -p bin 
--- xorshift/xorshift.cpp
+++ xorshift/xorshift.cpp
@@ -3,20 +3,22 @@
 #include <cstdlib>
 #include <vector>
 #include <cstdint>
+#include <boost/multiprecision/cpp_int.hpp>
 
 using std::cout;
 using std::cin;
+using namespace boost::multiprecision;
 
-static uint32_t state32; // for xorshift32
+static unsigned int state32; // for xorshift32
 static uint64_t state64; // for xorshift 64
-static uint32_t state128[4]; // for xorshift 128
+static unsigned int state128[4]; // for xorshift 128
 
-static uint32_t xorshift32()
+static unsigned int xorshift32()
 {
     if (state32 == 0) 
         throw std::runtime_error("Invalid xorshift seed");
     
-    uint32_t x = state32;
+    unsigned int x = state32;
 
     #ifdef _DEBUG
     cout << "\033[1;31m State before first xorshift: " << x << "\033[0m\n";
@@ -54,13 +56,13 @@ static uint64_t xorshift64()
     return state64 = x;
 }
 
-static uint32_t xorshift128()
+static unsigned int xorshift128()
 {
     if (state128[0] == 0 && state128[1] == 0
         && state128[2] == 0 && state128[3] == 0) 
             throw std::runtime_error("Invalid xorshift seed");
 
-    uint32_t s, t = state128[3];
+    unsigned int s, t = state128[3];
     state128[3] = state128[2];
     state128[2] = state128[1];
     state128[1] = s = state128[0];
@@ -77,17 +79,22 @@ int main(int argc, char **argv)
         return 0;
     }
     int choice = atoi(argv[1]);
-    
-    
+    int1024_t number;
+
+
     switch (choice) {
         case 32:
-            state32 = static_cast<uint32_t>(atoi(argv[2]));
+            state32 = static_cast<unsigned int>(atoi(argv[2]));
 
             #ifdef _DEBUG
             cout << "\033[1;34m xorshift32 with seed " << state32 << "\033[0m\n";
             #endif 
 
-            cout << xorshift32() << "\n";
+            // Generate a big boy
+            for(int i = 0; i < 32; i++) {
+                number |= static_cast<int1024_t>(xorshift32()) << 32 * i;
+            }
+            cout << number << "\n";
             break;
         case 64:
             state64 = static_cast<uint64_t> (atol(argv[2]));
@@ -95,14 +102,17 @@ int main(int argc, char **argv)
             #ifdef _DEBUG
             cout << "\033[1;34m xorshift64 with seed " << state64 << "\033[0m\n";
             #endif 
-
-            cout << xorshift64() << "\n";
+            // Generate a big boy
+            for(int i = 0; i < 16; i++) {
+                number |= static_cast<int1024_t>(xorshift64()) << 64 * i;
+            }
+            cout << number << "\n";
             break;
         case 128:
-            state128[0] = static_cast<uint32_t>(atoi(argv[2]));
-            state128[1] = static_cast<uint32_t>(atoi(argv[3]));
-            state128[2] = static_cast<uint32_t>(atoi(argv[4]));
-            state128[3] = static_cast<uint32_t>(atoi(argv[5]));
+            state128[0] = static_cast<unsigned int>(atoi(argv[2]));
+            state128[1] = static_cast<unsigned int>(atoi(argv[3]));
+            state128[2] = static_cast<unsigned int>(atoi(argv[4]));
+            state128[3] = static_cast<unsigned int>(atoi(argv[5]));
 
             #ifdef _DEBUG
             cout << "\033[1;34m xorshift32 with seed {";
@@ -110,9 +120,14 @@ int main(int argc, char **argv)
             cout << " " << state128[2] << " " << state128[3];  
             cout << "}\033[0m\n";
             #endif 
-
-            cout << xorshift128() << "\n";
+            for(int i = 0; i < 32; i++) {
+                number |= static_cast<int1024_t>(xorshift128()) << 32 * i;
+            }
+            
+            
+            cout << number << "\n";
             break;
+
         default:
             cout << "Invalid algorithm\n";
     }
